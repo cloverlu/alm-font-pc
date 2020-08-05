17225071b96eb4c32c288cc60fa15982cbda36d0
sunhua <947545659@qq.com>
2020-08-05 14:47:51 +0800

    端口

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@
  * @Date: 2020-07-02 20:07:27
  * @Author: sunhua
  * @Describe:
- * @LastEditTime: 2020-08-05 11:09:18
+ * @LastEditTime: 2020-08-05 14:47:37
  * @FilePath: \alm-font-pc\vue.config.js
  */
 const path = require("path");
-const apihost = "http://20.147.168.82:9002";
+const apihost = "http://20.147.168.82:9001";
 // 作为配置文件，直接导出配置对象即可
 module.exports = {
   // 基本路径
